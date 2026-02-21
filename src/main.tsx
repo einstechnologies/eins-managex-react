f73215049ce1bb1ab9c1f9f526369d1da45d9dcb
EINS-SD-TANAYA <tanaya@eins.co.in>
2026-02-21 11:22:36 +0530

    FEAT : DI for tanstacjk State Mangement and Page Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import Navbar from './components/Navbar'
-import LayoutPage from './pages/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './App.css';
+import AppRoutes from './Approutes'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LayoutPage />
+    <BrowserRouter>   {/* ✅ ADD THIS */}
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+      </QueryClientProvider>
+    </BrowserRouter>  {/* ✅ ADD THIS */}
+
   </StrictMode>,
   // hello
 )
