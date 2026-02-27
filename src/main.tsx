7613e78f74dce1492a92379095bedd19afcd48db
EINS-SD-MOHAMMED <mohammed@eins.co.in>
2026-02-27 12:01:09 +0530

    CHG: move the import of index.css below all the import to prevent the override of bootstrap changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './App.css';
 import AppRoutes from './Approutes'
+import './index.css'
 
 const queryClient = new QueryClient();
 
