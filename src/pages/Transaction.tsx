6616c5740b2fb9deb07f6974629a01ce1b86a900
EINS-SD-VRUSHALI <vrushali@eins.co.in>
2026-03-04 10:21:05 +0530

    Create New Transaction.css file and import it into Transaction.tsx

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/Dashboard.css";
+import "../styles/Transaction.css";
 import "../index.css";
 
 
