587d64984d10f339db06ad6ae231f167aff8b653
EINS-SD-KUNAL <150328015+EINS-SD-KUNAL@users.noreply.github.com>
2026-03-06 16:49:15 +0530

    FEAT : Created Device Information and Time zone page. (#35)
    * FEAT : Created Device Information and Time zone page.
    
    * CHG : Refactor code
    
    * CHG : changes in id and htmlfor label names and remove extra spacing between the code
    
    ---------
    
    Co-authored-by: EINS-SD-KUNAL <eins@EINS-SD-KUNAL>

---
+++ src/components/DeviceInformation.tsx
@@ -0,0 +1,150 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import '../styles/DeviceInformation.css'
+
+function DeviceInfo() {
+  return (
+    <div className="row mt-3 g-3">
+      <div className="col-12 mt-3 g-3">
+        <div className="card device-user-card device-information">
+          <h6>
+            <b>Device Information</b>
+          </h6>
+
+          <div className="d-flex align-items-start gap-3 mt-4 flex-wrap">
+
+            <div className="col-12 col-md-3">
+              <fieldset className="device-information dev-info-text-input-group">
+                <label htmlFor="devicename">Device Name</label>
+                <input id="devicename" className="form-control" />
+              </fieldset>
+            </div>
+
+            <div className="d-flex gap-2">
+              <button className="btn-outline-soft-green">Get Info</button>
+              <button className="btn-outline-soft-red">&nbsp;Clear&nbsp;</button>
+            </div>
+
+            <div className="row g-4">
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Device Name</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Module Name</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Device Id</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Serial No.</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Time</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Mac</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Memory</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Lisence</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Network</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Version</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label device-information">Secbox Version</span>
+                  <span className="device-information-colon device-information">:</span>
+                  <span className="device-information-value device-information">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label">iDCloud Code</span>
+                  <span className="device-information-colon">:</span>
+                  <span className="device-information-value">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <div className="d-flex">
+                  <span className="device-information-label">Online</span>
+                  <span className="device-information-colon">:</span>
+                  <span className="device-information-value">NA</span>
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6 dev-if-mt device-information">
+                <div className="d-flex">
+                  <span className="device-information-label">Online Available</span>
+                  <span className="device-information-colon">:</span>
+                  <span className="device-information-value">NA</span>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default DeviceInfo;
