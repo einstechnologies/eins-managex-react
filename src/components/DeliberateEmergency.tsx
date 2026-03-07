6273744413e239f8cb48b132e08dcc8a51e8f806
EINS-SD-VRUSHALI <vrushali@eins.co.in>
2026-03-07 10:14:21 +0530

    Changes done into component pages of Decive features and DeviceFeature.css (#38)

--- src/components/DeliberateEmergency.tsx
+++ src/components/DeliberateEmergency.tsx
@@ -4,18 +4,38 @@ export function DeliberateEmergency() {
     return (
         <>
             <div className="col-lg-12">
-                <h6>
-                    <b>Deliberate Emergency</b>
-                </h6>
-                <div className="spaceBetweencontentdevicefeatr"></div>
-                <div className="spaceBetweencontentdevicefeatr"></div>
-                <div className="col-12 restartdiv">
-                    <div className='col-lg-4' style={{ color: "blue" }}>
-                        <label htmlFor="device">Communaction Mode : Default</label>
+                <div className="card hid-card">
+                    <h6>
+                        <b>Deliberate Emergency</b>
+                    </h6>
+                    <div className="spaceBetweencontentdevicefeatr"></div>
+                    <div className="col-12 restartdiv">
+                        <div className='col-lg-4 commonwidth'>
+                            <label htmlFor="device">Communication Mode : Default</label>
+                        </div>
+                        <div className='col-lg-4'></div>
                     </div>
-                </div>
-                <div className="col-12 restartdiv"></div>
-                <div className="col-12 restartdiv spacebottom">
+                    <div className="col-12 restartdiv"></div>
+
+                    <div className="col-12 restartdiv">
+                        <div className='col-lg-3'>
+                            <fieldset className='text-input-group commonwidth'>
+                                <label htmlFor="ExceptionMode">Exception Mode</label>
+                                <select id='ExceptionMode'>
+                                    <option value="">Select Mode</option>
+                                    <option value="Normal">Normal</option>
+                                    <option value="LockDown">Lock Down</option>
+                                    <option value="Emergency">Emergency (Always Open)</option>
+                                </select>
+                            </fieldset>
+                        </div>
+
+                        <div className='col-lg-1'>
+                            <button className="btn-outline-soft-green spacetop commonwidth">Save</button>
+                        </div>
+                        <div className='col-lg-4'></div>
+                    </div>
+                    {/* <div className="col-12 restartdiv spacebottom">
                     <div className='col-lg-1'>
                         <label htmlFor="device">Exception Mode</label>
                     </div>
@@ -49,8 +69,9 @@ export function DeliberateEmergency() {
                     <div className='col-lg-1'>
                         <button className="btn-outline-soft-green" style={{ width: "100%" }}>Save</button>
                     </div>
-                </div>
-            </div >
+                </div> */}
+                </div >
+            </div>
         </>
     );
 }
--- src/components/RestartDevice.tsx
+++ src/components/RestartDevice.tsx
@@ -4,28 +4,25 @@ export function RestartDevice() {
     return (
         <>
             <div className="col-lg-12">
-                <h6>
-                    <b>Restart Device</b>
-                </h6>
-                <div className="spaceBetweencontentdevicefeatr"></div>
-                <div className="spaceBetweencontentdevicefeatr"></div>
-                <div className="col-12 restartdiv">
-                    <div className='col-lg-4'></div>
-                    <div className='col-lg-3'>
-                        <fieldset className='text-input-group' style={{ width: "100%" }}>
-                            <label htmlFor="device">Device Name</label>
-                            <select id='device'>
-                                <option value="">Select Device</option>
-                            </select>
-                        </fieldset>
+                <div className="card hid-card">
+                    <h6>
+                        <b>Restart Device</b>
+                    </h6>
+                    <div className="spaceBetweencontentdevicefeatr"></div>
+                    <div className="row restartdiv">
+                        <div className='col-lg-3'>
+                            <fieldset className='text-input-group commonwidth'>
+                                <label htmlFor="device">Device Name</label>
+                                <input id='device' />
+                            </fieldset>
+                        </div>
+
+                        <div className='col-lg-1'>
+                            <button className="btn-outline-soft-green spacetop commonwidth">Restart</button>
+                        </div>
                     </div>
 
-                    <div className='col-lg-1'>
-                        <button className="btn-outline-soft-green" style={{ width: "100%" }}>Restart</button>
-                    </div>
-                    <div className='col-lg-4'></div>
                 </div>
-
             </div>
         </>
     );
