9f7518ccbe78e8aa8effdb5011a86fb18018e05d
EINS-SD-VRUSHALI <vrushali@eins.co.in>
2026-03-09 12:00:35 +0530

    Fix: Design changes and same done into css file (#47)
    * Fix : changes done into DragDropUpload for icons; earlier it is only for Music. Changes from this file bind into Setlogo.tsx
    
    * Fix: Design changes and same done into css file

--- src/components/DeviceFeature/DoorSensorSettings.tsx
+++ src/components/DeviceFeature/DoorSensorSettings.tsx
@@ -6,75 +6,82 @@ export function DoorSensorSettings() {
                     <h6>
                         <b>Door Sensor Settings</b>
                     </h6>
+
                     <div className="spaceBetweencontentdevicefeatr"></div>
-                    <div className="col-12 restartdiv">
-                        <div className='col-lg-1'>
 
-                            <label className="form-check-label" htmlFor="deviceToggle">
-                                Active
-                            </label>
+                    <div className="row restartdiv">
 
+                        {/* Active */}
+                        <div className='col-12 col-md-12 col-lg-2 spacetopmaxdiv'>
+                            <div className="no-nc-toggle spaceBetweencontentdevicefeatr">
+                                <span className="spanmarginbottom">Active :</span>
 
-                        </div>
-                        <div className='col-lg-1 toggle-bottom'>
-                            <div className="form-check form-switch">
-                                <input
-                                    className="form-check-input togglesize"
-                                    type="checkbox"
-                                    id="deviceToggle"
-                                />
+                                <div className="form-check form-switch spanmargin-left">
+                                    <label className="toggle-switch">
+                                        <input
+                                            id="rdoOSDP"
+                                            type="checkbox"
+                                        />
+                                        <span className="toggle-slider"></span>
+                                    </label>
+                                </div>
                             </div>
-                        </div>
-                        <div className='col-lg-2'></div>
-                        <div className='col-lg-2'>
-                            <div className="no-nc-toggle">
-                                <span>NO</span>
 
-                                <div className="form-check form-switch">
-                                    <input
-                                        className="form-check-input togglesize"
-                                        type="checkbox"
-                                        id="deviceToggle"
-                                    />
+                            <div className="no-nc-toggle spaceBetweencontentdevicefeatr spacetopmax ">
+                                <span className="spanmarginbottom">Smart Closure :</span>
+
+                                <div className="form-check form-switch TimebasedAntipass-toggle">
+                                    <label className="toggle-switch">
+                                        <input
+                                            id="rdoSmartClosure"
+                                            type="checkbox"
+                                        />
+                                        <span className="toggle-slider"></span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="no-nc-toggle spaceBetweencontentdevicefeatr spacetopmax">
+
+                                <span className="spanmarginbottom">
+                                    NO :
+                                    <span className="tooltip-containerDoor">
+                                        <i className="bi bi-info-circle info-iconDoor"></i>
+                                        <span className="custom-tooltipDoor">Normal Open</span>
+                                    </span>
+                                </span>
+
+                                <div className="form-check form-switch spanmargin-left">
+                                    <label className="toggle-switch">
+                                        <input id="rdoNO" type="checkbox" />
+                                        <span className="toggle-slider spanmarginbottom"></span>
+                                    </label>
                                 </div>
 
-                                <span>NC</span>
+                                <span className="spanmargin-left">
+                                    NC :
+                                    <span className="tooltip-containerDoor">
+                                        <i className="bi bi-info-circle info-iconDoor"></i>
+                                        <span className="custom-tooltipDoor">Normal Close</span>
+                                    </span>
+                                </span>
+
                             </div>
-                        </div>
-                        <div className='col-lg-2'></div>
-
-                        <div className='col-lg-1'>
-
-                            <label className="form-check-label" htmlFor="deviceToggle">
-                                Smart Closure
-                            </label>
-
 
                         </div>
-                        <div className='col-lg-1 toggle-bottom'>
-                            <div className="form-check form-switch">
-                                <input
-                                    className="form-check-input togglesize"
-                                    type="checkbox"
-                                    id="deviceToggle"
-                                />
-                            </div>
-                        </div>
 
-
-                    </div>
-                    <div className="col-lg-12"></div>
-                    <div className="col-lg-12"></div><div className="col-12"></div>
-                    <div className="col-12 restartdiv spacebottom">
-
-                        <div className='col-lg-4'>
-                            <fieldset className='text-input-group commonwidth'>
+                        {/* Active Time */}
+                        <div className='col-12 col-md-12 col-lg-3'>
+                            <fieldset className="text-input-group commonwidth">
                                 <label htmlFor="device">Active time (ms)</label>
-                                <input id='Activetime' value='3000' />
+                                <input id="Activetime" defaultValue="3000" />
                             </fieldset>
                         </div>
-                        <div className='col-lg-1 btnmarginleft'>
-                            <button className="btn-outline-soft-green commonwidth">Save</button>
+
+                        {/* Save Button */}
+                        <div className='col-12 col-md-12 col-lg-3 spacetop'>
+                            <button className="btn-outline-soft-green commonwidth">
+                                Save
+                            </button>
                         </div>
                     </div>
                 </div>
