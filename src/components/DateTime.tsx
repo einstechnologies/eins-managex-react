720979142e9fc73fa8422f2c18185649ced6dd00
EINS-SD-ROHIT <rohit@eins.co.in>
2026-03-09 16:15:27 +0530

    FIX : Added Connected Device Information to the Info, DateTime, and Timezone tabs.

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -1,5 +1,6 @@
 import '../styles/DateTime.css'
 import { useState } from "react";
+import ConnectedDevice from "../components/ConnectedDevice";
 
 function DateTime() {
 
@@ -24,35 +25,44 @@ function DateTime() {
     };
 
     return (
-        <div className="row mt-3 g-3">
-            <div className="col-12 mt-3 g-3 date-time dt-bottom">
-                <div className="card dt-user-card date-time">
-                    <h6>
-                        <b>Date/Time</b>
-                    </h6>
+        <>
+            <ConnectedDevice
+                Name="Main Door"
+                IP="192.168.1.201"
+                mac="00:18:9E:6A:B8:35"
+                terminalid="5382314504545"
+            />
+            <div className="row  g-3">
 
-                    <div className="row mt-4">
-                        <div className="col-12 col-md-3">
-                            <fieldset className="date-time dt-info-text-input-group">
-                                <label htmlFor="currentdate">Current Date (MM/DD/YYYY)</label>
-                                <input id="currentdate" className="form-control" value={date} readOnly />
-                            </fieldset>
-                        </div>
-                        <div className="col-12 col-md-3">
-                            <fieldset className="date-time dt-info-text-input-group">
-                                <label htmlFor="currenttime">Current Time (HH:MM:SS)</label>
-                                <input id="currenttime" className="form-control" value={time} readOnly />
-                            </fieldset>
-                        </div>
-                    </div>
+                <div className="col-12 mt-3 g-3 date-time dt-bottom">
+                    <div className="card dt-user-card date-time">
+                        <h6>
+                            <b>Date/Time</b>
+                        </h6>
 
-                    <div className="date-time btn-datetime mt-3">
-                        <button className="date-time dt-btn-outline-soft-blue" onClick={handleGetDateTime}>Get Current Date Time</button>
-                        <button className="date-time dt-btn-outline-soft-green ms-2">Save</button>
+                        <div className="row mt-4">
+                            <div className="col-12 col-md-3">
+                                <fieldset className="date-time dt-info-text-input-group">
+                                    <label htmlFor="currentdate">Current Date (MM/DD/YYYY)</label>
+                                    <input id="currentdate" className="form-control" value={date} readOnly />
+                                </fieldset>
+                            </div>
+                            <div className="col-12 col-md-3">
+                                <fieldset className="date-time dt-info-text-input-group">
+                                    <label htmlFor="currenttime">Current Time (HH:MM:SS)</label>
+                                    <input id="currenttime" className="form-control" value={time} readOnly />
+                                </fieldset>
+                            </div>
+                        </div>
+
+                        <div className="date-time btn-datetime mt-3">
+                            <button className="date-time dt-btn-outline-soft-blue" onClick={handleGetDateTime}>Get Current Date Time</button>
+                            <button className="date-time dt-btn-outline-soft-green ms-2">Save</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
